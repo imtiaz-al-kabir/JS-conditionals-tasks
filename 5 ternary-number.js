bd59bd8df340c79e22895b0da823e14338cbebaa
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-07-31 15:39:42 +0600

    turnary problem complete

--- 5 ternary-number.js
+++ 5 ternary-number.js
@@ -10,3 +10,19 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+
+let num1 = 20;
+let num2 = 30;
+let result;
+if (num1 > num2) {
+  result = num1 * 2;
+  console.log(result);
+} else {
+  result = num1 + num2;
+  console.log(result);
+}
+let num3 = 20;
+let num4 = 30;
+let result2;
+const output = num3 > num4 ? (result2 = num3 * 2) : (result2 = num3 + num4);
+console.log(output);
