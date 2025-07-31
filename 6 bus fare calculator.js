c19738066a55845280205f653b2ab7c97fd2b564
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-07-31 15:58:59 +0600

    buss fare complete

--- 6 bus fare calculator.js
+++ 6 bus fare calculator.js
@@ -6,3 +6,21 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+let age = 20;
+isStudent = true;
+const regularFare = 800;
+let fare;
+
+if (age < 10) {
+  console.log("Free Ticket for Children");
+}
+if (isStudent) {
+  fare = regularFare * 0.5;
+  console.log(fare);
+}
+if (age >= 60) {
+  fare = regularFare * 0.15;
+  console.log(fare);
+} else {
+  console.log("Regular ticket fare 800 tk");
+}
